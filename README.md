328fece548629aaa9dd3a416d232aa7d889140fa
Vitali Baranov <vitali.baranov@gmail.com>
2017-11-19 12:20:09 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,6 +1,24 @@
-# Night Owls Detector
+# Night Owls Detector 
 
-[TODO. There will be project description]
+Prints the list of users of DevMan that have sent solutions of exercises after 24.00.
+
+# Quick Start
+Python 3.5 should be utilized.
+
+Example of run on Linux
+```bash
+$python3 seek_dev_nigthers.py
+Midnighters of DevMan are:
+1 id6250441
+2 andrewnorilsk
+3 gdath100500
+4 kadantsev
+5 yuriykustov
+6 maruschin
+7 SergeiKhrustalev
+8 Seosvet
+9 joj1337
+```
 
 # Project Goals
 
--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,15 +1,35 @@
-def load_attempts():
-    pages = 1
-    for page in range(pages):
-        # FIXME подключить загрузку данных из API
-        yield {
-            'username': 'bob',
-            'timestamp': 0,
-            'timezone': 'Europe/Moscow',
-        }
+import datetime
+import requests
+import pytz
 
-def get_midnighters():
-    pass
+
+def load_attempts():
+    devman_api_url = "https://devman.org/api/challenges/solution_attempts/"
+    attempts_pages = requests.get(devman_api_url).json()['number_of_pages']
+    for page in range(1, attempts_pages+1):
+        attempts_page_url = '{}?page={}'.format(devman_api_url, page)
+        attempts_list = requests.get(attempts_page_url).json()['records']
+        for attempt in attempts_list:
+            yield attempt
+
+
+def get_midnighters(attempts):
+    night_period_hours = range(7)
+    midnighters = set()
+    for attempt in attempts:
+        time_zone = pytz.timezone(attempt['timezone'])
+        attempt_time = datetime.datetime.fromtimestamp(
+                                                       attempt['timestamp'],
+                                                       time_zone
+                                                       )
+        if attempt_time.hour in night_period_hours:
+            midnighters.add(attempt['username'])
+    return midnighters
+
 
 if __name__ == '__main__':
-  pass
+    all_attempts = load_attempts()
+    midnighters = get_midnighters(all_attempts)
+    print('Midnighters of DevMan are:')
+    for index, midnighter in enumerate(midnighters, 1):
+        print(index, midnighter)
